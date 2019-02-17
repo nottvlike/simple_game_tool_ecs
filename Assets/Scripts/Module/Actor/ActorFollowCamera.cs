1a8856993d7be7b24eb8a92856898335fa776428
nottvlike <nottvlike@gmail.com>
2019-02-17 18:28:26 +0800

    优化 follow camera module

--- Assets/Scripts/Module/Actor/ActorFollowCamera.cs
+++ Assets/Scripts/Module/Actor/ActorFollowCamera.cs
@@ -5,11 +5,8 @@ using Data;
 
 namespace Module
 {
-    public class ActorFollowCamera : Module, ITimerObject
+    public class ActorFollowCamera : UpdateModule
     {
-        TimerEvent _timer;
-        Tweener _tweener;
-
         protected override void InitRequiredDataType()
         {
             _requiredDataTypeList.Add(typeof(FollowCameraData));
@@ -22,17 +19,6 @@ namespace Module
             return false;
         }
 
-        protected override void OnEnable()
-        {
-            _timer = WorldManager.Instance.TimerMgr.AddEndLess(0, 0, this);
-        }
-
-        protected override void OnDisable()
-        {
-            _timer.Clear();
-            _timer = null;
-        }
-
         public override void Refresh(ObjectData objData)
         {
             var resourceStateData = objData.GetData<ResourceStateData>();
@@ -41,34 +27,10 @@ namespace Module
                 return;
             }
 
-            if (_tweener != null && _tweener.IsPlaying)
-            {
-                return;
-            }
-
             var resourceData = objData.GetData<ResourceData>();
-            var transform = resourceData.gameObject.transform;
-
-            var screenPosition = Camera.main.WorldToScreenPoint(transform.position);
-            var maxWidth = Constant.SCREEN_WIDTH * Constant.MAX_FOLLOW_REGION / 2 ;
-            var maxHeight = Constant.SCREEN_HEIGHT * Constant.MAX_FOLLOW_REGION / 2 ;
-
-            var width = Mathf.Abs(screenPosition.x - Constant.SCREEN_WIDTH / 2);
-            var height = Mathf.Abs(screenPosition.y - Constant.SCREEN_HEIGHT / 2);
-
-            if (width > maxWidth || height > maxHeight)
-            {
-                var startPosition = Camera.main.transform.localPosition;
-                var endPosition = transform.localPosition;
-                endPosition.z = startPosition.z;
-                _tweener = TweenerUtil.Move(Camera.main.gameObject, 0, Constant.CAMERA_FOLLOW_INTERVAL, startPosition, endPosition);
-            }
-        }
-
-        public void Tick()
-        {
-            var player = WorldManager.Instance.Player;
-            Refresh(player);
+            var position = resourceData.gameObject.transform.position;
+            position.z = Camera.main.transform.position.z;
+            Camera.main.transform.position = position;
         }
     }
 }
